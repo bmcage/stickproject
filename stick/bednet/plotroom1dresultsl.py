a95b0a05e255feaaf19830d0e7b50c13761db7df
Benny Malengier <benny.malengier@gmail.com>
2014-01-31 15:22:01 +0000

    More plotting options

--- stick/bednet/plotroom1dresultsl.py
+++ stick/bednet/plotroom1dresultsl.py
@@ -33,19 +33,46 @@ import matplotlib.pyplot as plt
 import math
 from numpy import pi
 
-FILETOLOAD = '/home/benny/stickproject/fabricmuslin_Deet.ini/bednetroom1d_solpart_'
-ARG = '%05d.npz'
-INDEX = [0,1,2] # what dumped data to load
-EVERY = 2    # what time data to skip to reduce plotting time
+BASEDIR = '/home/benny/stickproject/'
+PROBS = True  #set tot False if only one problem
+PROBTOLOAD = 'fabricmuslin_Deet.ini_25nmol1day'
+#PROBTOLOAD = 'fabricbednetY335_Deet.ini_50nmol8hour'
+#all problems must be over the same grid !
+PROBSTOLOAD = ['fabricbednetY335_Deet.ini_50nmol8hour', 
+    'fabricbednetY335_Deet.ini_100nmol8hour', 
+    'fabricbednetY335_Deet.ini_200nmol8hour', 
+    'fabricbednetY335_Deet.ini_400nmol8hour']
+LABELS = ['50 nmol', '100 nmol', '200 nmol', '400 nmol']
+#PROBSTOLOAD = ['fabricmuslin_Deet.ini_25nmol2min_b',
+#    'fabricmuslin_Deet.ini_20nmol2min_b', 'fabricmuslin_Deet.ini_15nmol2min_b',
+#    'fabricmuslin_Deet.ini_10nmol2min_b', 'fabricmuslin_Deet.ini_05nmol2min_b']
+##LABELS = ['25 nmol', '20 nmol', '15 nmol', '10 nmol', '5 nmol']
+ARG = '/bednetroom1d_solpart_%05d.npz'
+INDEX = range(29) #range(4) # what dumped data to load
+EVERY = 60 #1    # what time data to skip to reduce plotting time
 
-times = np.empty(0, float)
-sol = None
-yarnmass = None
-totyarnmass = np.empty(0, float)
-totroommass = np.empty(0, float)
+#determine at what distance in mm to plot concentration over time: 
+x0 = [1, 5, 1000]
 
-for index in INDEX:
-    firstfile = FILETOLOAD + ARG % index
+double = True
+
+if not PROBS:
+    PROBSTOLOAD = [PROBTOLOAD]
+
+prob = 0
+ltimes = []
+ltotyarnmass = []
+ltotroommass = []
+lyarnmass = []
+lsol = []
+for PROBTOLOAD in PROBSTOLOAD:
+  times = np.empty(0, float)
+  sol = None
+  yarnmass = None
+  totyarnmass = np.empty(0, float)
+  totroommass = np.empty(0, float)
+  for index in INDEX:
+    firstfile = BASEDIR + PROBTOLOAD + ARG % index
     if not os.path.isfile(firstfile):
         print ('Unexisting file %s' % firstfile)
         sys.exit()
@@ -62,7 +89,6 @@ for index in INDEX:
         saturation_conc = tresh_sat[0]
         treshold = tresh_sat[1]
         grid = data['grid_cellcenters'] #self.grid
-        print (data['yarnmass'])
             
         if yarnmass is None:
             yarnmass = [0] * len(data['yarnmass'])
@@ -72,11 +98,14 @@ for index in INDEX:
             yarnmass[ind] = np.append(yarnmass[ind], dt[::EVERY])  #self.yarnmass
         totyarnmass = np.append(totyarnmass, data['totyarnmass'][::EVERY]) #self.totyarnmass
         totroommass = np.append(totroommass, data['totroommass'][::EVERY]) #self.totroommass
-
-print (times, yarnmass)
-#determine at what distance in mm to plot concentration over time: 
-x0 = [1]
-
+  #times are up to max time, not up to end of simulation, so trim
+  ltimes += [times]
+  lsol += [sol]
+  ltotyarnmass += [totyarnmass]
+  ltotroommass += [totroommass]
+  lyarnmass += [yarnmass]
+  prob += 1
+#now we determine how to interpolate over the grid for x0
 plotdata = []
 for xplot in x0:
     assert grid[0] < xplot < grid[-1], "%f < %f < %f "\
@@ -87,13 +116,19 @@ for xplot in x0:
             interpol_start = (xcell-xplot)/(grid[ind]-grid[ind-1])
             plotdata.append((ind-1, interpol_start))
             break
-
-def view_sol(times, sol):
+colors = 'bgrkmy'
+lencolors = len(colors)
+color = 0
+def view_sol(times, sol, label=None):
+    global colors, lencolors, color
     ind = 0
-    for ind, interpdat in enumerate(plotdata):
-        xval = x0[ind]
+    usecolor = colors[color%lencolors]
+    color += 1
+    for dataind, interpdat in enumerate(plotdata):
+        xval = x0[dataind]
         cellstart, interpval = interpdat
-        conc_in_point = interpval * sol[:, ind] + (1-interpval) * sol[:, ind+1]
+        conc_in_point = (interpval * sol[:, dataind] 
+                            + (1-interpval) * sol[:, dataind+1] )
         print ('conc in end point', conc_in_point[-1])
         plt.rc("font", family="serif")
         plt.rc("font", size=10)
@@ -108,15 +143,61 @@ def view_sol(times, sol):
         plt.gca().set_ylabel('Concentration [$\mu$g/mm$^3$]')
         #plt.gca().yaxis.set_major_formatter(pylab.FormatStrFormatter('%e'))
         plt.title('Concentration at position %g mm' % xval)
-        plt.plot(times, conc_in_point)
+        plt.plot(times, conc_in_point, label=label)
         #plt.ylim(0, maxv*1.1)
         plt.plot(times, np.ones(len(times)) * saturation_conc, 'k--')
         plt.plot(times, np.ones(len(times)) * treshold, 'b--')
+        plt.legend()
         plt.draw()
+        ind += 1
+        #same plot in unit minutes !
+        plt.rc("font", family="serif")
+        plt.rc("font", size=10)
+        width = 4.5  #width in inches
+        height = 1.4 #height in inches
+        plt.rc("figure.subplot", left=(50/72.27)/width)
+        plt.rc("figure.subplot", right=(width-10/72.27)/width)
+        plt.rc("figure.subplot", bottom=(14/72.27)/height)
+        plt.rc("figure.subplot", top=(height-7/72.27)/height)
+        plt.figure(ind)
+        plt.gca().set_xlabel('Time [min]')
+        plt.gca().set_ylabel('Concentration [$\mu$g/mm$^3$]')
+        #plt.gca().yaxis.set_major_formatter(pylab.FormatStrFormatter('%e'))
+        plt.title('Concentration at position %g mm' % xval)
+        plt.plot(times/60, conc_in_point, usecolor, label=label)
+        if double:
+            plt.plot(times/60, 2*conc_in_point, usecolor+'--', label='2 x '+label)
+        #plt.ylim(0,  treshold*1.1)
+        ##plt.plot(times/60, np.ones(len(times)) * saturation_conc, 'k--')
+        plt.plot(times/60, np.ones(len(times)) * treshold, 'b--')
+        plt.legend()
+        plt.draw()
+        ind +=1
+        #same plot in unit hour !
+        plt.rc("font", family="serif")
+        plt.rc("font", size=10)
+        width = 4.5  #width in inches
+        height = 1.4 #height in inches
+        plt.rc("figure.subplot", left=(50/72.27)/width)
+        plt.rc("figure.subplot", right=(width-10/72.27)/width)
+        plt.rc("figure.subplot", bottom=(14/72.27)/height)
+        plt.rc("figure.subplot", top=(height-7/72.27)/height)
+        plt.figure(ind)
+        plt.gca().set_xlabel('Time [hour]')
+        plt.gca().set_ylabel('Concentration [$\mu$g/mm$^3$]')
+        #plt.gca().yaxis.set_major_formatter(pylab.FormatStrFormatter('%e'))
+        plt.title('Concentration at position %g mm' % xval)
+        plt.plot(times/60/60, conc_in_point, label=label)
+        plt.plot(times/60/60, np.ones(len(times)) * saturation_conc, 'k--')
+        plt.plot(times/60/60, np.ones(len(times)) * treshold, 'b--')
+        plt.legend()
+        plt.draw()
+        ind +=1
+        
         #plt.savefig('AIconc_%03.1f_mm' % xval + '.png')
     return ind
 
-def view_sol_mass(ind):
+def view_sol_mass(ind, times, yarnmass, totyarnmass, totroommass, label=None):
     """
     Plot the evolution of the mass at current state of the solution
     """
@@ -135,34 +216,43 @@ def view_sol_mass(ind):
         plt.gca().set_xlabel('Time [s]')
         plt.gca().set_ylabel('Mass [$\mu$g]')
         plt.title('Mass AC in yarn type %d' % ind)
-        print (len(times), len(ymass))
-        plt.plot(times, ymass)
+        plt.plot(times, ymass, label=label)
         fignr += 1
 
     plt.figure(fignr)
-    plt.gca().set_xlabel('Time [s]')
+    plt.gca().set_xlabel('Time [h]')
     plt.gca().set_ylabel('Mass [$\mu$g]')
-    plt.title('Mass AC in the bednet')
+    plt.title('Mass AC in the textile')
     #plt.plot([0,],[28.935,], 'r*')
-    plt.plot(times, totyarnmass)
+    plt.plot(times/60/60, totyarnmass, label=label)
     fignr += 1
     plt.figure(fignr)
     plt.gca().set_xlabel('Time [s]')
     plt.gca().set_ylabel('Mass [$\mu$g]')
     plt.title('Mass AC in the room')
-    plt.plot(times, totroommass)
+    plt.plot(times, totroommass, label=label)
     fignr += 1
     #plot to check mass conservation
     plt.figure(fignr)
     plt.gca().set_xlabel('Time [s]')
     plt.gca().set_ylabel('Mass [$\mu$g]')
     plt.title('Total Mass AC')
-    plt.plot(times, totroommass+totyarnmass)
+    plt.plot(times, totroommass+totyarnmass, label=label)
     return fignr
 
 # we plot the data
 plt.ion()
-ind = view_sol(times, sol)
-view_sol_mass(ind)
+if len(lsol) == len(LABELS):
+    pass
+else:
+    print (len(lsol), len(LABELS))
+    LABELS = [None] * len(sol)
+
+for times, sol, label in zip(ltimes, lsol, LABELS):
+    ind = view_sol(times, sol, label)
+    
+for times, yarnmass, totyarnmass, totroommass, label in zip(ltimes, lyarnmass,
+                                        ltotyarnmass, ltotroommass, LABELS):
+    view_sol_mass(ind, times, yarnmass, totyarnmass, totroommass, label)
 
 plt.show(block=True) #block=True)
